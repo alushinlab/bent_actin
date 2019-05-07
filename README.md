bb15d1310115697b21e2c7752d9215111bc5d1f4
Greg <mreynolds@rockefeller.edu>
2019-05-07 11:48:04 -0400

    second commit: added first few files

--- README.md
+++ README.md
@@ -1,1 +1,152 @@
-# bent_actin
+# Bending Actin Project
+This repository stores the files for Matt's bent actin project. It is currently
+organized into four different sections:
+1) make\_bent\_actin: creates 
+2) make\_synthetic\_data
+3) train\_neural\_network
+4) neural\_network\_predictions
+
+## 1) make_bent_actin
+In make\_bent\_actin, there are two provided scripts:
+- generate\_bent\_actin\_streamlined.py - generates the actin filament
+- generate\_bent\_actin\_helper\_fxns.py - contains helper functions called
+by generate\_bent\_actin\_streamlined.py
+
+**To use:**
+
+Have both python files in your current directory, make generate\_bent\_actin\_streamlined.py 
+executable, have 'actin\_28mer\_centered.pdb' in your current directory, and 
+enter the following command:
+```
+./generate_bent_actin_streamlined.py --subunits numSubunits --radius radCurv --outputFileName out.pdb --plotting True
+```
+Where:
+- numSubunits = the number of total subunits for this bent actin filament. MUST BE ODD
+- radCurv = the radius of curvature for this bent actin. Typically a float between 150.0 to 100000.0; radCurv may be negative
+- outputFileName = Output file name. If not specified, will default to bent_actin_NN_subunits_RRR_curvature.pdb
+- plotting = Set to False if you do not want to see centroids plot. Leave blank or True otherwise
+ 
+**Required to run:**
+
+A PDB file of actin in your current directory. Currently, it must be called 'actin\_28mer\_centered.pdb' 
+but this can be changed in the future if we want to use different models.
+
+python 2.7;
+numpy;
+matplotlib;
+tqdm;
+scipy;
+ProDy;
+
+I have found it convenient to create an anaconda virtual environment with python version 2.7 and 
+use pip to install these libraries (scipy and matplotlib should come with numpy). 
+There are certainly other ways to install these libraries, but this way worked for me.
+
+**Known Bugs:**
+
+Currently none with this code. However, if the numSubunits is greater than 33, 
+Chimera will not render the PDB properly, probably because it is too large. All 
+of the information is still properly in the PDB file, it is a rendering issue 
+with Chimera. PDBs with very large subunit numbers may be viewed in pymol.
+
+## 2) make_synthetic_data
+In make\_synthetic\_data there is one executable python file called 'eman2\_projections.py.'
+This script requires as input a directory containing an arbitrary number of .mrc files.
+It currently rotates the MRC file in plane and translates in x,y,z using EMAN2 
+transformation functions. Then it convolves the MRC volume with a CTF using a SPARX 
+function, with a randomly chosen defocus value. Then the volume is projected in Z
+and noise is added in Fourier space to the 2D projection. 
+
+I generated my .mrc files by creating several .pdb files, as described above, and 
+converting them to .mrc format using EMAN2's pdb2mrc function. You may also use 
+chimera to make these maps if you so desire:
+```
+pdb2mrc input.pdb output.mrc apix=1.03 res=2.5 box=512 
+```
+Where:
+- input.pdb = path to input PDB file to be turned into an MRC file
+- output.mrc = output file name to save MRC file to
+- apix = pixel size in angstroms per voxel
+- res = resolution to which the .mrc file will be lowpass filtered
+- box = box size in voxels
+
+**Although this script is fully functional, it is still a work in progress**
+
+Still TODO: 
+1) make as executable command line script requiring user input to specify where to put
+input folder name and output folder names for noisy and non-noisy data
+2) also could use input to specify how many projections to make
+3) could run one single script in parallel, but requires careful attention for json files;
+can be done. I just need to take the time to do it.
+4) Instead of using actin model number that was loaded in, give the file name for this param
+
+**To Run eman2_projections.py.**
+```
+./eman2_projections.py
+```
+**Outputs**
+- Noisy 2D projections
+- Noiseless 2D projections
+- json files storing parameters corresponding to that projection (you can take 
+the specified actin file and apply these parameters to generate that projection).
+
+Because I am currently only trying to find large conformational changes, I have 
+found it useful to lowpass filter my noiseless 2D projections to 15 angstroms.
+To do so, I copy my projections to a new directory and run:
+```
+for file in ./*
+do
+proc3d ${file} ${file} lp=15 apix=1.03 norm
+done
+```
+This command will lowpass filter each projection to 15 angstroms and normalize it.
+
+**Required to run:**
+
+python 2.7;
+numpy;
+matplotlib;
+tqdm;
+scipy;
+
+ProDy;
+sparx;
+EMAN2;
+mrcfile;
+
+## 3) train_neural_network
+This script trains a contractive, denoising autoencoder to reconstruct a noiseless
+image from a noisy input. At a future date, I will include the neural network's 
+architecture, but for now I will say that I have seven outer convolutional layers
+followed by three inner dense/fully-connected layers to encode a noisy image 
+to a 128-dimensional vector, and the mirror of those layers is used to decode that
+vector. 
+The network is trained by first greedily training the convolutional layers until 
+just before overfitting, then I train the inner layers for one epoch. Lastly, I 
+train the full network on a large dataset.
+
+**The script for this is still under development. I will update the README.md documentation when it is close to being in its final form**
+
+**Required to run:**
+
+python 2.7;
+numpy;
+matplotlib;
+tqdm;
+scipy;
+
+tensorflow-gpu;
+keras (using tensorflow as backend);
+
+mrcfile;
+
+## 4) neural_network_predictions
+Currently this code is at the end of the previous script. I will decouple these 
+two scripts in my next iteration so that I can first train a network, and then 
+once that network is trained, I can use it to make predictions and see my encoded 
+representations of input images. 
+
+
+
+
+
---
+++ make_bent_actin/generate_bent_actin_helper_fxns.py
@@ -0,0 +1,162 @@
+################################################################################
+# imports
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from mpl_toolkits.mplot3d import Axes3D
+from prody import *
+from scipy.misc import derivative
+################################################################################
+# This function returns the points on a parametrically defined helix. The helix
+# parameterization is given by x = r*cos(omega*t + phi) + d1
+#										 y = r*sin(omega*t + phi) + d2
+#										 z = c*t
+# where t is in radians and r,c,phi,omega,d1,d2,d3,radius are the params. 
+# t_rad should be a list of integers defining the actin subunit number
+def evaluate_curve_pts(params, t_rad):
+	return np.array([(params[0]*np.cos(params[3]*t_rad + params[2]*np.pi/180.0)+params[4]), 
+						  (params[0]*np.sin(params[3]*t_rad + params[2]*np.pi/180.0)+params[5]), 
+						  (params[1]*t_rad+params[6]+t_rad)]).T
+
+################################################################################
+# Evaluate where the centroids of the curvy points go
+def evaluate_curve_pts_curvy(params, t_rad, radius):
+	s = (params[1]*t_rad)
+	y_disp = params[0]*np.sin(params[3]*t_rad + params[2]*np.pi/180.0)+ params[5]
+	return np.array([(params[0]*np.cos(params[3]*t_rad + params[2]*np.pi/180.0)) + params[4], 
+						  -1.0*(-1.0*y_disp*np.cos(s/radius) + radius*(1-np.cos(s/radius))),
+						  params[6]+radius*np.sin(s/radius)+y_disp*np.sin(s/radius)]).T
+
+################################################################################
+# Functions to compute frenet-serret basis vectors for a simple helix. 
+# TODO: Do this numerically using a differentiation function to verify I can get
+# same results for when I do this for an arbitary function
+def calc_unit_tangent_vect(params, t_rad):
+	tangent_vect = np.array([(-1.0*params[0]*params[3])*np.sin(params[3]*t_rad + params[2]*np.pi/180.0), 
+									 (params[0]*params[3])*np.cos(params[3]*t_rad + params[2]*np.pi/180.0), 
+									 params[1]*np.ones(len(t_rad))]).T
+	for i in range(0, len(tangent_vect)):
+		tangent_vect[i] = tangent_vect[i] / np.linalg.norm(tangent_vect[i])
+	
+	return tangent_vect
+
+def calc_unit_normal_vect(params, t_rad):
+	normal_vect = np.array([(-1.0*params[0]*params[3]*params[3])*np.cos(params[3]*t_rad + params[2]*np.pi/180.0), 
+									 (-1.0*params[0]*params[3]*params[3])*np.sin(params[3]*t_rad + params[2]*np.pi/180.0), 
+									 0.0*np.ones(len(t_rad))]).T
+	for i in range(0, len(normal_vect)):
+		normal_vect[i] = normal_vect[i] / np.linalg.norm(normal_vect[i])
+	
+	return normal_vect
+
+def calc_unit_binormal_vect(tangent_vect, normal_vect):
+	binormal_vect  = []
+	for i in range(0, len(tangent_vect)):
+		binormal_vect.append(np.cross(tangent_vect[i],normal_vect[i]))
+	
+	return np.asarray(binormal_vect)
+
+################################################################################
+# Functions for calculating Frenet-Serret bases of curved helix
+from scipy.misc import derivative
+def x_of_t(t, r,c,phi,omega,d1,d2,d3,radius):
+	return r*np.cos(omega*t + phi*np.pi/180.0) + d1
+
+def y_of_t(t, r,c,phi,omega,d1,d2,d3,radius):
+	s = (c*t)
+	y_disp = r*np.sin(omega*t +phi*np.pi/180.0)+ d2
+	return -1.0*(-1.0*y_disp*np.cos(s/radius) + radius*(1-np.cos(s/radius)))
+
+def z_of_t(t, r,c,phi,omega,d1,d2,d3,radius):
+	s = (c*t)
+	y_disp = r*np.sin(omega*t + phi*np.pi/180.0)+ d2
+	return d3+radius*np.sin(s/radius)+y_disp*np.sin(s/radius)
+
+def gammat_of_t(t, r,c,phi,omega,d1,d2,d3,radius):
+	return np.array([x_of_t(t, r,c,phi,omega,d1,d2,d3,radius), 
+						  y_of_t(t, r,c,phi,omega,d1,d2,d3,radius),
+						  z_of_t(t, r,c,phi,omega,d1,d2,d3,radius)]).T
+
+def d_gamma_dt(t, r,c,phi,omega,d1,d2,d3,radius):
+	dx_dt = derivative(x_of_t,t,dx=1e-4, order=15, args=(r,c,phi,omega,d1,d2,d3,radius))
+	dy_dt = derivative(y_of_t,t,dx=1e-4, order=15, args=(r,c,phi,omega,d1,d2,d3,radius))
+	dz_dt = derivative(z_of_t,t,dx=1e-4, order=15, args=(r,c,phi,omega,d1,d2,d3,radius))
+	return np.array([dx_dt, dy_dt, dz_dt])
+
+def calc_unit_tangent_vect_curvy(t, r,c,phi,omega,d1,d2,d3,radius):
+	f_t = d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius)[0] / np.linalg.norm(d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius),axis=0)
+	g_t = d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius)[1] / np.linalg.norm(d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius),axis=0)
+	h_t = d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius)[2] / np.linalg.norm(d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius),axis=0)
+	return np.array([f_t, g_t, h_t]).T
+
+def T_x(t,r,c,phi,omega,d1,d2,d3,radius):
+	return d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius)[0] / np.linalg.norm(d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius),axis=0)
+
+def T_y(t,r,c,phi,omega,d1,d2,d3,radius):
+	return d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius)[1] / np.linalg.norm(d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius),axis=0)
+
+def T_z(t,r,c,phi,omega,d1,d2,d3,radius):
+	return d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius)[2] / np.linalg.norm(d_gamma_dt(t,r,c,phi,omega,d1,d2,d3,radius),axis=0)
+
+
+def d_T_dt(t,r,c,phi,omega,d1,d2,d3,radius):
+	df_dt = derivative(T_x,t,dx=1e-4, order=15, args=(r,c,phi,omega,d1,d2,d3,radius))
+	dg_dt = derivative(T_y,t,dx=1e-4, order=15, args=(r,c,phi,omega,d1,d2,d3,radius))
+	dh_dt = derivative(T_z,t,dx=1e-4, order=15, args=(r,c,phi,omega,d1,d2,d3,radius))
+	return np.array([df_dt, dg_dt, dh_dt])
+
+def calc_unit_normal_vect_curvy(t,r,c,phi,omega,d1,d2,d3,radius):
+	f_t = d_T_dt(t,r,c,phi,omega,d1,d2,d3,radius)[0] / np.linalg.norm(d_T_dt(t,r,c,phi,omega,d1,d2,d3,radius),axis=0)
+	g_t = d_T_dt(t,r,c,phi,omega,d1,d2,d3,radius)[1] / np.linalg.norm(d_T_dt(t,r,c,phi,omega,d1,d2,d3,radius),axis=0)
+	h_t = d_T_dt(t,r,c,phi,omega,d1,d2,d3,radius)[2] / np.linalg.norm(d_T_dt(t,r,c,phi,omega,d1,d2,d3,radius),axis=0)
+	return np.array([f_t, g_t, h_t]).T
+
+def calc_unit_binormal_vect_curvy(tangent_vect, normal_vect):
+	binormal_vect  = []
+	for i in range(0, len(tangent_vect)):
+		binormal_vect.append(np.cross(tangent_vect[i],normal_vect[i]))
+	
+	return np.asarray(binormal_vect)
+
+################################################################################
+def make_curved_filament(p, T_helix, N_helix, B_helix, T_helix_curved, N_helix_curved, B_helix_curved, pts_helix, pts_helix_curved, num_subunits):
+	actin_orig_0 = p.select('chain C').copy()
+	actin_newRef_0 = actin_orig_0.copy()
+	a_T = T_helix[0]; a_N = N_helix[0]; a_B = B_helix[0]; a_0 = pts_helix[0] 
+	M_0 = np.column_stack((a_T,a_N,a_B))
+	coords_orig_0 = actin_orig_0.getCoords()
+	frenet_serret_frame_actin = []
+	for i in range(0, len(coords_orig_0)):
+		set_to_zero = coords_orig_0[i] - np.average(actin_orig_0.getCoords(), axis=0)
+		frenet_serret_frame_actin.append(np.matmul(M_0.T,set_to_zero))
+	
+	new_chains = []
+	bent_p = 0
+	for i in range(0, num_subunits):
+		b_T=T_helix_curved[i]; b_N=N_helix_curved[i]; b_B=B_helix_curved[i]; b_0 = pts_helix_curved[i]
+		M_1 = np.column_stack((b_T,b_N,b_B))
+		coords_new_0 = []
+		for j in range(0, len(coords_orig_0)):
+			rot = np.matmul(M_1,frenet_serret_frame_actin[j])
+			rot_shift = rot + b_0
+			coords_new_0.append(rot_shift)
+		
+		coords_new_0 = np.asarray(coords_new_0)
+		temp_chain = actin_orig_0.copy()
+		temp_chain.setCoords(coords_new_0)
+		new_chains.append(temp_chain)
+		if(bent_p == 0):
+			bent_p = temp_chain
+		
+		else:
+			bent_p = bent_p + temp_chain
+		
+		#showProtein(new_chains[i])
+	
+	#plt.show()
+	return bent_p
+
+
+
+
+
